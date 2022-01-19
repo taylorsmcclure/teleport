9a50cc7c8d5f14c41a07fc5edd941b1d6e0b9278
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2022-01-19 16:53:35 +0100

    Update `OIDCConnectorV2` to `OIDCConnectorV3`

--- api/client/client.go
+++ api/client/client.go
@@ -1326,11 +1326,11 @@ func (c *Client) GetOIDCConnectors(ctx context.Context, withSecrets bool) ([]typ
 
 // UpsertOIDCConnector creates or updates an OIDC connector.
 func (c *Client) UpsertOIDCConnector(ctx context.Context, oidcConnector types.OIDCConnector) error {
-	oidcConnectorV2, ok := oidcConnector.(*types.OIDCConnectorV2)
+	oidcConnectorV3, ok := oidcConnector.(*types.OIDCConnectorV3)
 	if !ok {
 		return trace.BadParameter("invalid type %T", oidcConnector)
 	}
-	_, err := c.grpc.UpsertOIDCConnector(ctx, oidcConnectorV2, c.callOpts...)
+	_, err := c.grpc.UpsertOIDCConnector(ctx, oidcConnectorV3, c.callOpts...)
 	return trail.FromGRPC(err)
 }
 
